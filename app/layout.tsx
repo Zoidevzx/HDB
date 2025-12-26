f5f917a67c2907fec6f40d07d5219950e463b5ab
Zoidevzx <opala2019@gmail.com>
2025-12-26 16:40:14 -0300

    Adicionar animação de transição

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import Music from '@/components/Music'
 import "./globals.css";
 import { Pacifico, Fredoka, Montserrat } from 'next/font/google'
 
